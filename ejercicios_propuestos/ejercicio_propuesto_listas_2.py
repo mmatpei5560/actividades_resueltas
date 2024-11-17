61ef1d04ed4ea5e1718400d1057d4721f8864d01
Mauricio Matamala <mmatpei560@g.educaand.es>
2024-11-17 22:08:09 +0100

    Renombrado de variables en ejercicio_propuesto_listas_2

--- ejercicios_propuestos/ejercicio_propuesto_listas_2.py
+++ ejercicios_propuestos/ejercicio_propuesto_listas_2.py
@@ -1,6 +1,6 @@
 # Ejercicio propuesto con listas (2)
 
-lista_empleados = []
+lista_comerciales = []
 nombre = input("Introduzca el nombre del comercial: ")
 while (nombre != ""):
     comercial_actual = []
@@ -10,16 +10,16 @@ while (nombre != ""):
     comercial_actual.append(int(input("Venta(X): ")))
     comercial_actual.append(int(input("Venta(J): ")))
     comercial_actual.append(int(input("Venta(V): ")))
-    lista_empleados.append(comercial_actual)
+    lista_comerciales.append(comercial_actual)
     nombre = input("\nIntroduzca el nombre del comercial: ")
 
-print("\n","-"*40,"\nINFORMACIÓN DE DEPURACIÓN: lista_empleados = ", lista_empleados,"\n","-"*40,"\n")
+print("\n","-"*40,"\nINFORMACIÓN DE DEPURACIÓN: lista_empleados = ", lista_comerciales,"\n","-"*40,"\n")
 
 num_items = int(input("Introduzca el número de items de interés: "))
 print("Los empleados que vendieron ", num_items, " items fueron:")
-for empleado in lista_empleados:
-    if num_items in empleado:
-        nombre_empleado = empleado[0]
-        print("- ", nombre_empleado)
+for comercial in lista_comerciales:
+    if num_items in comercial:
+        nombre_comercial = comercial[0]
+        print("- ", nombre_comercial)
 
         
